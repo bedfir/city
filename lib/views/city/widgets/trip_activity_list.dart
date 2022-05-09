1b3cde681a9640451f3d923dae8828e6586674b7
Bedoui <firas-bedoui@hotmail.fr>
2022-05-09 23:33:07 +0200

    refactorizer tripActivityList

--- lib/views/city/widgets/trip_activity_list.dart
+++ lib/views/city/widgets/trip_activity_list.dart
@@ -1,1 +1,12 @@
 import 'package:flutter/material.dart';
+
+class TripActivityList extends StatelessWidget {
+  const TripActivityList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Jesuis la'),
+    );
+  }
+}
