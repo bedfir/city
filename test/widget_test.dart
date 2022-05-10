059e529ba03fa6e8b5f9b8665c25d02527e72f4e
Bedoui <firas-bedoui@hotmail.fr>
2022-05-10 14:19:53 +0200

    test screenHome

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,23 +8,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:material/main.dart';
+import 'package:material/views/city/city.dart';
 
 void main() {
-  /*testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const DymaTrip());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });*/
+  // Define a test. The TestWidgets function also provides a WidgetTester
+  // to work with. The WidgetTester allows building and interacting
+  // with widgets in the test environment.
+  testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
+    // Create the widget by telling the tester to build it.
+    await tester.pumpWidget(MaterialApp(home: City()));
+  });
 }
