9471503496047219792e3d5a76b2f2ea3e4aeab4
Bedoui <firas-bedoui@hotmail.fr>
2022-05-11 10:56:39 +0200

    comment this class

--- lib/models/activity.model.dart
+++ lib/models/activity.model.dart
@@ -1,3 +1,18 @@
+/// A class with four properties, one named constructors.
+///
+/// This class take four requeired parameters, that describe the [Activity].
+///
+/// {@tool snippet}
+/// This next exemple show the properties
+///
+/// ```dart
+/// String name;
+/// String image;
+/// String id;
+/// String city;
+/// ```
+/// {@end-tool}
+
 class Activity {
   String name;
   String image;
