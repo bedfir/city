d3884b331b0508e2c79001bca5c6a12f83a39b17
Bedoui <firas-bedoui@hotmail.fr>
2022-05-11 10:57:58 +0200

    comment this class

--- lib/views/city/widgets/activity_list.dart
+++ lib/views/city/widgets/activity_list.dart
@@ -2,6 +2,36 @@ import 'package:flutter/material.dart';
 import './activity_card.dart';
 import '../../../models/activity.model.dart';
 
+///  {@tool snippet}
+///
+/// The following is an exemple of a stateless widget
+/// subclass called `ActivityList`.
+///
+/// Normally, widgets have more constructor arguments, each of which corresponds
+/// to a `final` property.
+///
+/// The constructor required activities a List<[Activity]>
+///
+/// ```dart
+/// class GreenFrog extends StatelessWidget {
+///   const ActivityList({
+///   Key? key,
+///   List<Activity> required this.activities
+/// }) : super(key: key);
+///
+///   @override
+///   Widget build(BuildContext context) {
+///     return GridView(children: activities
+///          .map(
+///            (activity) => ActivityCard(
+///              activity: activity,
+///           ),
+///         ).toList(),
+///        );
+///     }
+/// }
+/// ```
+/// {@end-tool}
 class ActivityList extends StatelessWidget {
   final List<Activity> activities;
   const ActivityList({
