41a4311c82b6b890cc3859dc12ed9afa9c410068
Bedoui <firas-bedoui@hotmail.fr>
2022-05-11 10:56:53 +0200

    comment this class

--- lib/models/trip.model.dart
+++ lib/models/trip.model.dart
@@ -1,7 +1,22 @@
+///{@tool snippet}
+/// ```dart
+///class Trip {
+///  String city;
+///  List<String> activities;
+///  DateTime date;
+///  Trip({
+///    required this.city,
+///    required this.activities,
+///    required this.date,
+///  });
+///}
+/// ```
+/// {@end-tool}
+
 class Trip {
   String city;
   List<String> activities;
-  DateTime date;
+  DateTime? date;
   Trip({
     required this.city,
     required this.activities,
