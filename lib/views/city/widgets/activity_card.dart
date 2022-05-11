e11b528191738d18d96d55dd97de32040a1d8b73
Bedoui <firas-bedoui@hotmail.fr>
2022-05-11 12:51:32 +0200

    add widget Stack, Ink, Rows

--- lib/views/city/widgets/activity_card.dart
+++ lib/views/city/widgets/activity_card.dart
@@ -3,16 +3,64 @@ import '../../../models/activity.model.dart';
 
 class ActivityCard extends StatelessWidget {
   final Activity activity;
-  const ActivityCard({Key? key, required this.activity}) : super(key: key);
+  final bool isSelected;
+  final VoidCallback toggleActivity;
+  const ActivityCard({
+    Key? key,
+    required this.activity,
+    required this.isSelected,
+    required this.toggleActivity,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: double.infinity,
       height: 150,
-      child: Image.asset(
-        activity.image,
-        fit: BoxFit.cover,
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Ink.image(
+            image: AssetImage(activity.image),
+            fit: BoxFit.cover,
+            child: InkWell(
+              onTap: toggleActivity,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      if (isSelected)
+                        const Icon(
+                          Icons.check,
+                          size: 40,
+                          color: Colors.white,
+                        ),
+                    ],
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Flexible(
+                      child: Text(
+                        activity.name,
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
