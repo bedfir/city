25b1f675ef0aeb627b918ae8cddf55ee9b2e3e13
Bedoui <firas-bedoui@hotmail.fr>
2022-05-12 22:25:10 +0200

    update tester to check  home view

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:material/views/city/city_view.dart';
+import 'package:material/views/home/home_view.dart';
 
 void main() {
   // Define a test. The TestWidgets function also provides a WidgetTester
@@ -16,6 +17,6 @@ void main() {
   // with widgets in the test environment.
   testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
     // Create the widget by telling the tester to build it.
-    await tester.pumpWidget(MaterialApp(home: CityView()));
+    await tester.pumpWidget(MaterialApp(home: HomeView()));
   });
 }
